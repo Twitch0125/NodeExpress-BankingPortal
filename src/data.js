23a636ff71dfcdb499816940203506c625e3e99f
Kaleb Ercanbrack <kaleb@pop-os.localdomain>
2019-06-14 18:07:14 -0600

    typo

--- src/data.js
+++ src/data.js
@@ -1,6 +1,13 @@
 const fs = require("fs");
 const path = require("path");
 
+const accountData = fs.readFileSync(
+  path.join(__dirname, "json", "accounts.json"),
+  "utf8"
+);
+
+const accounts = JSON.parse(accountData);
+
 const userData = fs.readFileSync("src/json/users.json", { encoding: "utf8" });
 
 const users = JSON.parse(userData);
